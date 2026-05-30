06a160f394d0e068b472ae7d74d350c45d2b1437
michaelsong5713 <kvgfcmxmqx@privaterelay.appleid.com>
2026-05-30 10:17:12 -0400

    first drafT

---
+++ main.py
@@ -0,0 +1,100 @@
+import torch
+import torch.nn as nn
+from torchvision import datasets
+from torchvision.transforms import v2
+from torch.utils.data import random_split
+from torch.utils.data import DataLoader
+
+training_data = datasets.CIFAR10(
+    root = "data",
+    download = True,
+    train = True
+)
+testing_data = datasets.CIFAR10(
+    root = "data",
+    download = True,
+    train = False
+)
+
+mean = [0.4914, 0.4822, 0.4465]
+std  = [0.2470, 0.2435, 0.2616]
+
+transform = v2.Compose([v2.ToImage(), v2.ToDtype(torch.float32, scale=True),v2.Normalize(mean = mean, std = std)])
+training_data.transform = transform
+testing_data.transform = transform
+
+training_len = len(training_data)
+train_size = int(0.85 * training_len)
+val_size = int(0.15 * training_len)
+training_dataset,validation_dataset = random_split(
+    training_data,
+    [train_size,val_size]
+)
+
+train_dataloader = DataLoader(training_dataset, batch_size=64, shuffle=True)
+val_dataloader = DataLoader(validation_dataset,batch_size=64, shuffle=False)
+test_dataloader = DataLoader(testing_data,batch_size=64, shuffle = False)
+
+class conv_block(nn.Module):
+    def __init__(self,in_ch,out_ch):
+        super().__init__()
+        self.block = nn.Sequential(
+            nn.Conv2d(in_ch,out_ch,kernel_size=3,padding=1),
+            nn.BatchNorm2d(out_ch),
+            nn.ReLU()
+        )
+    def forward(self,x):
+        x = self.block(x)
+        return x
+
+class neural_network(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.features = nn.Sequential(
+            conv_block(3,16),
+            conv_block(16,32)
+        )
+        self.classifier = nn.Sequential(
+            nn.AdaptiveAvgPool2d((1,1)),
+            nn.Flatten(),
+            nn.Linear(32,16),
+            nn.ReLU(),
+            nn.Linear(16,10)
+        )
+    def forward(self,x):
+        x = self.features(x)
+        x = self.classifier(x)
+        return x
+
+generations = 3
+model = neural_network()
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(),lr=0.01)
+
+for i in range(generations):
+    model.train()
+    for batch_d,batch_l in train_dataloader:
+        output = model(batch_d)
+        #print(f"{output.shape}")
+        loss = loss_fn(output,batch_l)
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+        #print(f"Train Loss: {loss}\n")
+    model.eval()
+    with torch.no_grad():
+        for batch_d,batch_l in val_dataloader:
+            output = model(batch_d)
+            loss = loss_fn(output,batch_l)
+            #print(f"Val Loss: {loss}\n")
+
+correct = 0
+total = 0
+with torch.no_grad():
+    for batch_d,batch_l in val_dataloader:
+        output = model(batch_d)
+        prediction = torch.argmax(output,dim=1)
+        correct += torch.sum(prediction==batch_l)
+        total+=prediction.size(0)
+accuracy = correct/total*100
+print(f"Accuracy: {accuracy}\n")
